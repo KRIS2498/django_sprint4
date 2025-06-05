4854415b28bd480d32e0c628c60c2717b921930e
Kris Zotkina <zotkina.kris@mail.ru>
2025-06-05 17:33:29 +0300

    oshibki2

--- blogicum/blog/utils.py
+++ blogicum/blog/utils.py
@@ -1,22 +1,17 @@
-from blog.constants import DEFAULT_NUM_PAGE, POSTS_ON_PAGE
 from blog.models import Post
-from django.core.paginator import Paginator, Page
+from django.core.paginator import Paginator
 from django.db.models import Count
 from django.utils import timezone
-from django.http import HttpRequest
-from django.db import models
 
 
-def posts_pagination(
-    request: HttpRequest,
-    queryset: models.QuerySet,
-    per_page: int = POSTS_ON_PAGE,
-    page_key: str = 'page',
-    default_page: int = DEFAULT_NUM_PAGE
-) -> Page:
-    paginator = Paginator(queryset, per_page)
-    page_number = request.GET.get(page_key, default_page)
+def get_page_from_paginator(paginator, page_number):
     return paginator.get_page(page_number)
+
+
+def posts_pagination(request, query_set, per_page=10):
+    paginator = Paginator(query_set, per_page)
+    page_number = request.GET.get('page')
+    return get_page_from_paginator(paginator, page_number)
 
 
 def query_post(
@@ -33,9 +28,11 @@ def query_post(
         )
     if with_comments:
         queryset = annotate(queryset)
-    return queryset.order_by('-pub_date')
+    return queryset
 
 
 def annotate(queryset):
     """Аннотации количества комментариев к постам"""
-    return queryset.annotate(comment_count=Count('comments'))
+    return queryset.annotate(
+        comment_count=Count('comments')
+    ).order_by('-pub_date')
